fc86f71ce0049d32f5ab0a8a0c721b90f0d65f41
VillifCoder559 <francesco.villi@gmail.com>
2023-09-12 17:51:06 +0200

    fix

--- Texture.cpp
+++ Texture.cpp
@@ -1,109 +1,48 @@
-#include"Texture.h"
+#include "Texture.h"
 
-Texture::Texture(const char* image, const char* texType, GLuint slot)
-{
-	//type of the texture
-	type = texType;
+Texture::Texture(const char *image, const char *texType, GLuint slot) {
+  glGenTextures(1, &ID);
+  int width, height, nrComponents;
+  unsigned char *data = stbi_load(image, &width, &height, &nrComponents, 0);
+  // std::cout<<"W:" <<width<<"H:"<<height<<std::endl;
+  if (data) {
+    type = texType;
+    unit = slot;
+    GLenum format;
+    if (nrComponents == 1)
+      format = GL_RED;
+    else if (nrComponents == 3)
+      format = GL_RGB;
+    else if (nrComponents == 4)
+      format = GL_RGBA;
 
-	int widthImg, heightImg, numColCh;
-	// Flip the image so it appears right side up
-	stbi_set_flip_vertically_on_load(true);
-	// Read the image from a file and stores it in bytes
-	unsigned char* bytes = stbi_load(image, &widthImg, &heightImg, &numColCh, 0);
+    glBindTexture(GL_TEXTURE_2D, ID);
+    glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
 
-	glGenTextures(1, &ID);
-	glActiveTexture(GL_TEXTURE0 + slot);
-	unit = slot;
-	glBindTexture(GL_TEXTURE_2D, ID);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-	// Check what type of color channels the texture has and load it accordingly
-	if (type == "normal") // la normale va messa solo in rgb
-		glTexImage2D
-		(
-			GL_TEXTURE_2D,
-			0,
-			GL_RGB,
-			widthImg,
-			heightImg,
-			0,
-			GL_RGBA,
-			GL_UNSIGNED_BYTE,
-			bytes
-		);
-	else if (numColCh == 4)
-		glTexImage2D
-		(
-			GL_TEXTURE_2D,
-			0,
-			GL_SRGB_ALPHA,
-			widthImg,
-			heightImg,
-			0,
-			GL_RGBA,
-			GL_UNSIGNED_BYTE,
-			bytes
-		);
-	else if (numColCh == 3)
-		glTexImage2D
-		(
-			GL_TEXTURE_2D,
-			0,
-			GL_SRGB,
-			widthImg,
-			heightImg,
-			0,
-			GL_RGB,
-			GL_UNSIGNED_BYTE,
-			bytes
-		);
-	else if (numColCh == 1)
-		glTexImage2D
-		(
-			GL_TEXTURE_2D,
-			0,
-			GL_SRGB,
-			widthImg,
-			heightImg,
-			0,
-			GL_RED,
-			GL_UNSIGNED_BYTE,
-			bytes
-		);
-	else
-		throw std::invalid_argument("Automatic Texture type recognition failed");
-
-
-
-	stbi_image_free(bytes);
-	glBindTexture(GL_TEXTURE_2D, 0);
+    stbi_image_free(data);
+  } else {
+    std::cout << "Texture failed to load at path: " << image << std::endl;
+    stbi_image_free(data);
+  }
 }
 
-void Texture::texUnit(Shader& shader, const char* uniform, GLuint unit)
-{
-	// Gets the location of the uniform
-	GLuint texUni = glGetUniformLocation(shader.ID, uniform);//send to shader the info of diffuse map as a uniform called diffuse0
-	shader.Activate();
-	glUniform1i(texUni, unit);
+void Texture::texUnit(Shader &shader, const char *uniform, GLuint unit) {
+  // shader needs to be activated before changing the value of a uniform
+  shader.Activate();
+  GLuint texUni = glGetUniformLocation(shader.ID, uniform);
+  glUniform1i(texUni, unit);
 }
 
-void Texture::Bind()
-{
-	glActiveTexture(GL_TEXTURE0 + unit);
-	glBindTexture(GL_TEXTURE_2D, ID);
+void Texture::Bind() {
+  glActiveTexture(GL_TEXTURE0 + unit);
+  glBindTexture(GL_TEXTURE_2D, ID);
 }
 
-void Texture::Unbind()
-{
-	glBindTexture(GL_TEXTURE_2D, 0);
-}
-
-void Texture::Delete()
-{
-	glDeleteTextures(1, &ID);
-}
+void Texture::Unbind() { glBindTexture(GL_TEXTURE_2D, 0); }
+void Texture::Delete() { glDeleteTextures(1, &ID); }
